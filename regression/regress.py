db397a054151fb9cf1e3b8f3150daf4265d1a058
Weichen Li <weichenli@cmu.edu>
2018-04-20 01:52:27 -0400

    modularized python utils

--- regression/regress.py
+++ regression/regress.py
@@ -3,238 +3,130 @@ import sys
 from parsimony import *
 
 
-def main():
-    #os.chdir("E:\\Google Drive\\My Workspace\\USA\\BIC\\Spring 2017 courses\\02604 Bio\\Coding Assignments")
-    # os.chdir("C:\\Users\\Blade\\Google Drive\\My Workspace\\USA\\BIC\\Spring 2017 courses\\02604 Bio\\Coding Assignments")
-    '''
-    with open("Week 10\\dataset_38506_12.txt", "r") as data:
-        n = int(data.readline().strip())
-        T = {}
-        assign = {}
-        counter = 0
-        character_list = []
+def get_neighbor_pair(line, assign, counter):
+    division = line.strip().split("->")
+
+    if division[0].isdigit() and division[1].isdigit():
+        father = int(division[0])
+        son = int(division[1])
+    elif division[0].isdigit():
+        father = int(division[0])
+        b = division[1]
+        if b not in assign:
+            assign[b] = counter[0]
+            counter[0] += 1
+        son = assign[b]
+    elif division[1].isdigit():
+        a = division[0]
+        son = int(division[1])
+        if a not in assign:
+            assign[a] = counter[0]
+            counter[0] += 1
+        father = assign[a]
+    else:
+        a = division[0]
+        b = division[1]
+        if a not in assign:
+            assign[a] = counter[0]
+            counter[0] += 1
+        if b not in assign:
+            assign[b] = counter[0]
+            counter[0] += 1
+        father = assign[a]
+        son = assign[b]
+
+    return father, son
+
+
+def connect_neighbor_pair(father, son, T):
+    if father in T:
+        if son not in T[father]:
+            T[father].append(son)
+    else:
+        T[father] = [son]
+
+    if son in T:
+        if father not in T[son]:
+            T[son].append(father)
+    else:
+        T[son] = [father]
+
+
+def construct_char_list(character_list, assign):
+    for each in assign:
+        while len(character_list) < len(each):
+            character_list.append({})
+        for i in range(len(each)):
+            character_list[i][assign[each]] = each[i]
+
+
+def insert_root(T):
+    new_node = max([i for i in T]) + 1
+    left = [i for i in T][0]
+    right = T[left][0]
+
+    if len(T[left]) == 1:
+        del T[left]
+    else:
+        T[left] = [i for i in T[left] if i != right]
+
+    if len(T[right]) == 1:
+        del T[right]
+    else:
+        T[right] = [i for i in T[right] if i != left]
+
+    T.setdefault(left, []).append(new_node)
+    T.setdefault(right, []).append(new_node)
+    T.setdefault(new_node, []).append(left)
+    T[new_node].append(right)
+
+
+def read_lines(file_name):
+    result = []
+    with open(file_name, "r") as data:
         for line in data:
-            division = line.strip().split("->")
+            result.append(line.strip())
+    return result
 
-            if division[0].isdigit() and division[1].isdigit():
-                father = int(division[0])
-                son = int(division[1])
-            elif division[0].isdigit():
-                father = int(division[0])
-                b = division[1]
-                if b not in assign:
-                    assign[b] = counter
-                    counter += 1
-                son = assign[b]
-            elif division[1].isdigit():
-                a = division[0]
-                son = int(division[1])
-                if a not in assign:
-                    assign[a] = counter
-                    counter += 1
-                father = assign[a]
-            else:
-                a = division[0]
-                b = division[1]
-                if a not in assign:
-                    assign[a] = counter
-                    counter += 1
-                if b not in assign:
-                    assign[b] = counter
-                    counter += 1
-                father = assign[a]
-                son = assign[b]
 
-            if father in T:
-                if son not in T[father]:
-                    T[father].append(son)
-            else:
-                T[father] = [son]
+def write_result(file_name, result):
+    with open(file_name, "w") as output:
+        for i in result:
+            output.write(i)
 
-            if son in T:
-                if father not in T[son]:
-                    T[son].append(father)
-            else:
-                T[son] = [father]
 
-        for each in assign:
-            while len(character_list) < len(each):
-                character_list.append({})
-            for i in range(len(each)):
-                character_list[i][assign[each]] = each[i]
+def run_python_baseline(file_name, outfile_name):
+    T = {}
+    assign = {}
+    counter = [0]
+    character_list = []
 
-        new_node = max([i for i in T]) + 1
-        left = [i for i in T][0]
-        right = T[left][0]
+    lines = read_lines(file_name)
+    n = int(lines[0])
 
-        if len(T[left]) == 1:
-            del T[left]
-        else:
-            T[left] = [i for i in T[left] if i != right]
+    for line in lines[1:]:
+        father, son = get_neighbor_pair(line, assign, counter)
+        connect_neighbor_pair(father, son, T)
 
-        if len(T[right]) == 1:
-            del T[right]
-        else:
-            T[right] = [i for i in T[right] if i != left]
+    construct_char_list(character_list, assign)
 
-        T.setdefault(left, []).append(new_node)
-        T.setdefault(right, []).append(new_node)
-        T.setdefault(new_node, []).append(left)
-        T[new_node].append(right)
-
-    # Tree format
-    # T = {node: {"str": xxx, "children": [...]}, ...}
-    tree_list = []
-
-    total = 0
-    for each in character_list:
-        result, backtrack_tree, root = small_parsimony_one_tree_unrooted(copy.deepcopy(T), each)
-        tree_list.append(backtrack_tree)
-        total += result
-
-    combined_tree = {}
-
-    store = deque()
-    store.append(root)
-
-    while len(store) > 0:
-        cur = store.popleft()
-        str_list = [each[cur]["str"] for each in tree_list]
-        combined_tree.setdefault(cur, {})["str"] = "".join(str_list)
-        combined_tree[cur]["children"] = tree_list[0][cur]["children"]
-        for son in combined_tree[cur]["children"]:
-            store.append(son)
-
-    with open("result.txt", "w") as data:
-        data.write("{}\n".format(total))
-        store = deque()
-        store.append(root)
-        while len(store) > 0:
-            cur = store.popleft()
-            cur_str = combined_tree[cur]["str"]
-            for son in combined_tree[cur]["children"]:
-                son_str = combined_tree[son]["str"]
-                data.write("{}->{}:{}\n".format(cur_str, son_str, hamming_distance(cur_str, son_str)))
-                data.write("{}->{}:{}\n".format(son_str, cur_str, hamming_distance(cur_str, son_str)))
-                store.append(son)
-    '''
-
-    '''
-    with open("Week 10\\dataset_38507_6.txt", "r") as data:
-        options = data.readline().strip().split(" ")
-        a = int(options[0])
-        b = int(options[1])
-        T = {}
-        for line in data:
-            cur = line.strip().split("->")
-            father = int(cur[0])
-            son = int(cur[1])
-            if father in T:
-                if son not in T[father]:
-                    T[father].append(son)
-            else:
-                T[father] = [son]
-
-            if son in T:
-                if father not in T[son]:
-                    T[son].append(father)
-            else:
-                T[son] = [father]
-
-    T_1, T_2 = nearest_neighbors(a, b, T)
-
-    with open("result.txt", "w") as output:
-        for i in T_1:
-            for j in T_1[i]:
-                output.write("{}->{}\n".format(i, j))
-        output.write("\n")
-        for i in T_2:
-            for j in T_2[i]:
-                output.write("{}->{}\n".format(i, j))
-    '''
-
-    with open("../data/dataset_38507_8.txt", "r") as data:
-        n = int(data.readline().strip())
-        T = {}
-        assign = {}
-        counter = 0
-        character_list = []
-        for line in data:
-            division = line.strip().split("->")
-
-            if division[0].isdigit() and division[1].isdigit():
-                father = int(division[0])
-                son = int(division[1])
-            elif division[0].isdigit():
-                father = int(division[0])
-                b = division[1]
-                if b not in assign:
-                    assign[b] = counter
-                    counter += 1
-                son = assign[b]
-            elif division[1].isdigit():
-                a = division[0]
-                son = int(division[1])
-                if a not in assign:
-                    assign[a] = counter
-                    counter += 1
-                father = assign[a]
-            else:
-                a = division[0]
-                b = division[1]
-                if a not in assign:
-                    assign[a] = counter
-                    counter += 1
-                if b not in assign:
-                    assign[b] = counter
-                    counter += 1
-                father = assign[a]
-                son = assign[b]
-
-            if father in T:
-                if son not in T[father]:
-                    T[father].append(son)
-            else:
-                T[father] = [son]
-
-            if son in T:
-                if father not in T[son]:
-                    T[son].append(father)
-            else:
-                T[son] = [father]
-
-        for each in assign:
-            while len(character_list) < len(each):
-                character_list.append({})
-            for i in range(len(each)):
-                character_list[i][assign[each]] = each[i]
-
-        new_node = max([i for i in T]) + 1
-        left = [i for i in T][0]
-        right = T[left][0]
-
-        if len(T[left]) == 1:
-            del T[left]
-        else:
-            T[left] = [i for i in T[left] if i != right]
-
-        if len(T[right]) == 1:
-            del T[right]
-        else:
-            T[right] = [i for i in T[right] if i != left]
-
-        T.setdefault(left, []).append(new_node)
-        T.setdefault(right, []).append(new_node)
-        T.setdefault(new_node, []).append(left)
-        T[new_node].append(right)
+    insert_root(T)
 
     # T is undirected, rooted
     # format: T : {a : [b, c, d], b: [a], ...}
-    result = nearest_neighbor_interchage(T, character_list)
+    total, result, combined_tree = run_small_parsimony_one_tree_unrooted(
+        T, character_list)
 
-    with open("result.txt", "w") as output:
-        for i in result:
-            output.write(i)
+    write_result(outfile_name, result)
+
+
+def main():
+    input_file = "../data/dataset_38507_8.txt"
+    python_outfile = "python_result.txt"
+    cpp_outfile = "cpp_result.txt"
+
+    run_python_baseline(input_file, python_outfile)
+
 
 if __name__ == '__main__':
     main()
---
+++ src/API.cpp
@@ -0,0 +1,20 @@
+SmallParsimony(shared_ptr<int> idx_arr, shared_ptr<int> children_arr,
+               shared_ptr<char> char_list, int num_char_trees, int num_nodes) {
+  /*
+   * idx_arr: store starting position of "children_arr".
+   * 		(all leaf id is at the very begining; total length is (n+1 (i.e. 1 is
+   * root)), with last one is root's index; leaves indices is -1) children_arr:
+   * store children node id. (length are always multiple of 2) e.g. idx_arr =
+   * [-1, -1, -1,-1, 0, 2, 4]; children_arr=[0,1, 2,3, 4,5]
+   *
+   * char_list: length is (total number of single-char_trees) X (number of node
+   * + 1(root)) e.g. if string length of a leaf if 3 and 2 leaves for each tree,
+   *      then char_list=[('A','C'), ('C', 'A'), ('G', 'T')] (i.e. each () is
+   * for one single char tree)
+   *
+   * num_char_trees: total number of single-char_trees, which is also the total
+   * length of a leaf's string.
+   *
+   * num_nodes: n + 1, which is total number of nodes, plus one root node.
+   */
+}
